3fb2bc2e34ff932ef6bffa9bc20c5fa520927761
saminshakya <saminshakya27@gmail.com>
2023-05-15 10:45:17 +0545

    Basic of sets, array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,12 +1,36 @@
 import UIKit
 
-var highScore = 0
-highScore = 100
 
-let myName = "Samin"
 
-var currentUser = 124
-currentUser = 1000
 
-var ages: [Int] = [10, 15 , 20 , 19 , 25, 30]
-ages.count
+var ages = [18,33,55,17,94,12,26,17,33]
+
+//var agesSet: Set<Int> = []
+
+var agesSet = Set(ages)
+
+agesSet.insert(107)
+agesSet.contains(107)
+
+print(agesSet)
+
+//var highScore = 0
+//highScore = 100
+//
+//let myName = "Samin"
+//
+//var currentUser = 124
+//currentUser = 1000
+//
+//var ages: [Int] = [10,10, 15 , 20 , 19 , 25, 30]
+//ages.count
+//ages.first
+//ages.last
+//ages[3]
+//
+//ages.append(99)
+//ages.insert(50, at: 0)
+//ages.sort()
+//ages.reverse()
+//ages.shuffle()
+//print(ages)
